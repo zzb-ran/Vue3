a2e079f9bfd3829cad278d6f5728a97167674e52
zzb-ran <zzbmnran@gmail.com>
2021-11-11 14:45:17 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-bcw4ya
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-bcw4ya)
---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p>Count: {{ count }}</p>
+    <button @click="addCount">+</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import { reactive, toRefs, watch } from 'vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  setup(props, context) {
+    const state = reactive({
+      count: 0,
+    });
+    const addCount = () => {
+      state.count += 1;
+    };
+    watch(
+      () => state.count,
+      (currentCount, prevCount) => {
+        console.log('currentCount:', currentCount);
+        console.log('prevCount:', prevCount);
+      }
+    );
+    return {
+      ...toRefs(state),
+      addCount,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
